1e8288788025110987ff0ad100183323094bf6ce
Dzmitry Dulik <dulikds@gmail.com>
2021-03-29 11:57:33 +0300

    Initial commit

---
+++ src/test/java/com/dulik/udemy/oauth/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulik.udemy.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
