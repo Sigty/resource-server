67b32714e54a76c6e51a720c354ac9be4d1c37fe
Dzmitry Dulik <dulikds@gmail.com>
2021-05-21 01:08:55 +0300

    Scope Based Access Control and add README.md file.

--- src/main/java/com/dulik/udemy/oauth/security/WebSecurity.java
+++ src/main/java/com/dulik/udemy/oauth/security/WebSecurity.java
@@ -1,22 +1,30 @@
 package com.dulik.udemy.oauth.security;
 
+import com.dulik.udemy.oauth.converter.KeycloakRoleConverter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/v1/info/echo")
-                .hasAuthority("SCOPE_profile")
+//                .hasAuthority("SCOPE_profile")
+                .hasRole("developer")
                 .anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
 }
